4db1172907ddea6955c7811471d92881e5e740d7
Sonia Pacheco <103387638+soniapachecomza@users.noreply.github.com>
2024-05-10 22:12:46 -0300

    primer proyecto astro

---
+++ src/components/HelloComponent.jsx
@@ -0,0 +1,13 @@
+export default function HelloComponent() {
+  return (
+    <div>
+        <h1>
+            React Hello
+        </h1>
+
+        <button>
+            Click here
+        </button>
+    </div>
+  )
+}
